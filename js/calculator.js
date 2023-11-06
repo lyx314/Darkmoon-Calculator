956cad3fac0e77ed33764d23e4a82ad3e21f6be0
lyx314 <1010369037@qq.com>
2023-11-06 23:43:35 +0800

    craft

--- js/calculator.js
+++ js/calculator.js
@@ -3,24 +3,25 @@
 import { BigNumber } from "./bignumber.js-9.1.1/bignumber.mjs";
 
 export class Calculator {
-    numbers = [];
+    #numbers = [];
+    #enemies = [];
 
     constructor(material) {
         material.numbers.forEach((num) =>
-            this.numbers.push(new BigNumber(num))
+            this.#numbers.push(new BigNumber(num))
         );
-    }
-
-    calcWeight(arr) {
-        let weight = BigNumber(arr[0]);
-        weight = weight.plus(BigNumber(arr[1]).multipliedBy(3));
-        weight = weight.plus(BigNumber(arr[2]).multipliedBy(9));
-        return weight;
     }
 
     get percent() {
         const totalWeight = 9999 * (1 + 3 + 9);
-        const result = this.calcWeight(this.numbers).dividedBy(totalWeight);
+        const result = this.#calcWeight(this.#numbers).dividedBy(totalWeight);
         return Number(result.multipliedBy(100).toFixed(2));
+    }
+
+    #calcWeight(arr) {
+        let weight = BigNumber(arr[0]);
+        weight = weight.plus(BigNumber(arr[1]).multipliedBy(3));
+        weight = weight.plus(BigNumber(arr[2]).multipliedBy(9));
+        return weight;
     }
 }
