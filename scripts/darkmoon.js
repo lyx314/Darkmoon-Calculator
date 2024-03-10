4be62c7b4948f795dcace4038bc1a6e1b4d27855
lyx314 <1010369037@qq.com>
2024-03-10 15:01:38 +0800

    每车材料，精确显示

--- scripts/darkmoon.js
+++ scripts/darkmoon.js
@@ -459,11 +459,11 @@ export class Darkmoon {
             <div class="materials-per-run">
                 每车材料
                 <img src="img/${this.dm.currentID}-2.png" />
-                ${calculator.materialsPerRun[2].toFixed(1)}
+                ${calculator.materialsPerRun[2]}
                 <img src="img/${this.dm.currentID}-1.png" />
-                ${calculator.materialsPerRun[1].toFixed(1)}
+                ${calculator.materialsPerRun[1]}
                 <img src="img/${this.dm.currentID}-0.png" />
-                ${calculator.materialsPerRun[0].toFixed(1)}
+                ${calculator.materialsPerRun[0]}
             </div>`;
 
         calculator.calculateRun(
